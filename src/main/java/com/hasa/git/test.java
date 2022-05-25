d5e6215d9db910ec5ff6d3c77d86f87554ad6f1a
hasa9527 <106169315+hasa9527@users.noreply.github.com>
2022-05-25 22:34:03 +0800

    pull commit

--- src/main/java/com/hasa/git/test.java
+++ src/main/java/com/hasa/git/test.java
@@ -6,5 +6,6 @@ public class test {
         System.out.println("hello git1");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
