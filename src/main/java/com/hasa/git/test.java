5d68359dfe94928f2b5e133b7bc465a46deba241
hasa <546633406@qq.com>
2022-05-25 21:22:54 +0800

    hot-fix commit

--- src/main/java/com/hasa/git/test.java
+++ src/main/java/com/hasa/git/test.java
@@ -3,5 +3,6 @@ package com.hasa.git;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
